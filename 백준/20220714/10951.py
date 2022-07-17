ccbc23768ec6d0f19a51c8cfaebf34b181d6272d
Ash <ashkim2001@gmail.com>
2022-07-18 08:28:30 +0900

    20220714 update

--- 백준/20220714/10951.py
+++ 백준/20220714/10951.py
@@ -1,6 +1,9 @@
 #!/usr/env/python3
 
-for _ in range(0, 5):
-    a,b = input().split(' ')
-    cur = int(a)+int(b)
-    print(str(cur))
+while True:
+    try:
+        a,b = input().split(' ')
+        cur = int(a)+int(b)
+        print(str(cur))
+    except:
+        break
