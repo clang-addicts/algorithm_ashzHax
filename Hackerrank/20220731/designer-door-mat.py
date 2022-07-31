9da95aa8eeebc30150baad73534bfc1760aa6f1c
Ash <ashkim2001@gmail.com>
2022-07-31 19:11:35 +0900

    20220731 partial commit

---
+++ Hackerrank/20220731/designer-door-mat.py
@@ -0,0 +1,19 @@
+arr = input().split(' ')
+y = int(arr[0])
+x = int(arr[1])
+
+for _ in range(0, int(y/2)):
+    mcnt = 1
+    #for _ in range(0, int(x/2)):
+    #for _ in range(0, mcnt):
+    #for _ in range(0, int(x/2)):
+
+print('*'*(int((x-7)/2)), end='')
+print('WELCOME', end='')
+print('*'*(int((x-7)/2)), end='')
+
+for _ in range(0, int(y/2)):
+    mcnt = int(y/2)
+    #for _ in range(0, int(x/2)):
+    #for _ in range(0, mcnt):
+    #for _ in range(0, int(x/2)):
---
+++ 백준/20220731/2675.py
@@ -0,0 +1,11 @@
+#!/usr/env/python3
+
+for a in range(0, int(input())):
+    arr = input().split(' ')
+    loop_cnt = int(arr[0]) 
+    string = arr[1]
+
+    new = ''
+    for g in list(string):
+        for _ in range(0, loop_cnt):
+            new = new + g
